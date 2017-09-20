46fcc6c83137dd872b3fb2f82552b49b61dcf0f6
EricSihongL <31781716+EricSihongL@users.noreply.github.com>
2017-09-19 20:23:43 -0700

    Create puzzle-7.js

---
+++ puzzle-7.js
@@ -0,0 +1,22 @@
+right();
+if(getColor()=="red") {
+  up();
+  up();
+  up();
+}
+else if(getColor()!="red") {
+  down();
+  down();
+  down();
+}
+
+if(getColor()=="red") {
+  left();
+  up();
+  down();
+}
+else if(getColor()=="blue") {
+  right();
+  up();
+  down();
+}
