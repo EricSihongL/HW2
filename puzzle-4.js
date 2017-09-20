f454e1684358945701f3dbb9d91ea4e4941bb0ee
EricSihongL <31781716+EricSihongL@users.noreply.github.com>
2017-09-19 20:23:11 -0700

    Create puzzle-4.js

---
+++ puzzle-4.js
@@ -0,0 +1,17 @@
+right();
+down();
+down();
+down();
+down();
+var firstColor = getColor();
+down();
+setColor(firstColor)
+right();
+right();
+up();
+right();
+var secondColor = getColor();
+down();
+setColor(secondColor)
+right();
+right();
